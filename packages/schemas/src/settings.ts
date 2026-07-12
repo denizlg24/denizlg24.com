aec3acabe5d8889f5bdb37e7b31a068d7fe1d5c5
Deniz Lopes Günes <denizlg24@gmail.com>
2026-07-12 15:05:16 +0100

    feat(cv): add cv management page to control uploaded cv

--- packages/schemas/src/settings.ts
+++ packages/schemas/src/settings.ts
@@ -10,3 +10,16 @@ export const appSettingsResponseSchema = z.object({
   settings: appSettingsSchema,
 });
 export type AppSettingsResponse = z.infer<typeof appSettingsResponseSchema>;
+
+export const cvFileSchema = z.object({
+  url: z.string(),
+  filename: z.string(),
+  size: z.number(),
+  updatedAt: z.string(),
+});
+export type ICvFile = z.infer<typeof cvFileSchema>;
+
+export const cvResponseSchema = z.object({
+  cv: cvFileSchema.nullable(),
+});
+export type CvResponse = z.infer<typeof cvResponseSchema>;
